a7b57c2d0334c2fdcbe51243fa31af39572cf9f5
Jagepard <dankorot@gmail.com>
2020-03-23 19:49:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+author  : Jagepard <jagepard@yandex.ru>
+license https://mit-license.org/ MIT
+"""
+
+from Item import Item
+from Client import Client
+from Iterator import Iterator
+
+client = Client()
+
+client.addItemToTheBucket(Item("Колготки", 150, "штопаные"))
+client.addItemToTheBucket(Item("Мясо", 250, "тухлое"))
+client.addItemToTheBucket(Item("Батон", 40, ""))
+
+employee = Iterator(client.getBucket());
+
+try:
+    employee.iterateItems();
+except:
+    print('An error occured.')
